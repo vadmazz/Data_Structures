474b267e11fc60a0a9c1305ff09c21fec721b084
vadmazz <vadmazz2013@yandex.ru>
2019-12-02 00:40:45 +0300

    Bubble Sort

---
+++ Algorithmes/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Data_and_Algoritmes.Algorithmes
+{   /// <summary>
+    /// Сортировка пузырьком.
+    /// Сложность: o(n*n). За каждый проход элементы сравниваются попарно,
+    /// пока не будет достигнут результат. 
+    /// </summary>
+    public class BubbleSort
+    {
+        /// <summary>
+        /// Выполняет сортировку
+        /// </summary>
+        /// <param name="m">Множество</param>
+        /// <typeparam name="T">Тип множества (должен реализовавыть IComparable)</typeparam>
+        public void Sort<T>(T[] m)
+            where T : IComparable<T>
+        {
+            for (int i = 0; i < m.Length; i++)
+            {
+                for (int j = i + 1; j < m.Length; j++)
+                {
+                    if (m[j].CompareTo(m[i]) < 0)
+                    {
+                        var temp = m[i];
+                        m[i] = m[j];
+                        m[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
